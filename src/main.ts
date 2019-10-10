7b96cab7ef6a5ff2fc165c217019fbe26733786d
Massimiliano Pippi <m.pippi@arduino.cc>
2019-10-10 14:43:18 +0200

    prettify code

--- src/main.ts
+++ src/main.ts
@@ -2,15 +2,15 @@ import * as core from "@actions/core";
 import * as installer from "./installer";
 
 async function run() {
-    try {
-        let version = core.getInput("version");
+  try {
+    let version = core.getInput("version");
 
-        if (version) {
-            await installer.getArduinoCli(version);
-        }
-    } catch (error) {
-        core.setFailed(error.message);
+    if (version) {
+      await installer.getArduinoCli(version);
     }
+  } catch (error) {
+    core.setFailed(error.message);
+  }
 }
 
 run();
